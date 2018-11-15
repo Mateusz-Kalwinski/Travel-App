33eb57574b56aaabc7aed38e91b9e521abdd38bf
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-11-15 11:59:24 +0100

    views,login and register done

---
+++ resources/lang/pl/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'article' => 'artykul',
+  'object' => 'objekt',
+  'person' => 'osoba',
+  'room' => 'pokoj',
+  'roomsearch' => 'szukajpokoju',
+  'myobjects' => 'mojeobiekty',
+  'saveobject' => 'zapiszobjekt',
+  'profile' => 'profil',
+  'saveroom' => 'zapiszpokoj',
+];
