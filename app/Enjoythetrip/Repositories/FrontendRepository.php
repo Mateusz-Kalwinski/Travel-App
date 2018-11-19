9e2dda350537bb2567d0c65a8e01e0f02af3f53b
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-11-19 10:55:47 +0100

    method return all tourist objects

---
+++ app/Enjoythetrip/Repositories/FrontendRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enjoythetrip\Repositories;
+
+use App\TouristObject;
+use App\Enjoythetrip\Interfaces\FrontendRepositoryInterface;
+
+class FrontendRepository implements FrontendRepositoryInterface{
+
+    public function getObjectsForMainPage(){
+
+        return TouristObject::all();
+    }
+
+}
