aa8a025d77a6e21e503301fd183db0226f116f59
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-11-19 10:59:21 +0100

    return city, photo, and object data to Repository

---
+++ app/TouristObject.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TouristObject extends Model
+{
+    protected $table = 'objects';
+
+    public function city(){
+
+        return $this->belongsTo('App\City');
+
+    }
+
+    public function photos(){
+
+        return $this->morphMany('App\Photo','photoable');
+
+    }
+}
