3a864aad4532102d04b8651f30d10e52854c5a6e
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-11-21 10:05:22 +0100

    looking for a city name like in input

--- app/Enjoythetrip/Repositories/FrontendRepository.php
+++ app/Enjoythetrip/Repositories/FrontendRepository.php
@@ -2,20 +2,34 @@
 
 namespace App\Enjoythetrip\Repositories;
 
-use App\TouristObject;
 use App\Enjoythetrip\Interfaces\FrontendRepositoryInterface;
+use App\{TouristObject,City};
 
-class FrontendRepository implements FrontendRepositoryInterface{
+class FrontendRepository implements FrontendRepositoryInterface  {
 
-    public function getObjectsForMainPage(){
-
-//        return TouristObject::all();
-        return TouristObject::with(['city', 'photos'])->ordered()->paginate(8);
+    public function getObjectsForMainPage()
+    {
+        // return TouristObject::all();
+        return TouristObject::with(['city','photos'])->ordered()->paginate(8);
     }
 
-    public function getObject($id){
 
-        return TouristObject::find($id);
+
+    public function getObject($id)
+    {
+        //return TouristObject::find($id);
+
+
+        // rooms.object.city   for json mobile because there is no lazy loading there
+        return  TouristObject::with(['city','photos', 'address','users.photos','rooms.photos','comments.user','articles.user','rooms.object.city'])->find($id);
     }
+
+
+
+    public function getSearchCities( string $term)
+    {
+        return  City::where('name', 'LIKE', $term . '%')->get();
+    }
+
 
 }
