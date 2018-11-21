c0ba8a3d8fc09466e9a2b958380859470ba513c0
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-11-21 10:12:53 +0100

    route to search cieties

--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,25 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-//Route::get('/', function () {
-//    return view('frontend.index');
-//});
-//
-//Route::get('/object', 'FrontendController@object')->name('object');
-//Route::get('/adminHome', 'FrontendController@adminHome')->name('adminHome');
-//Route::get('/rootSearch', 'FrontendController@roomSearch')->name('roomSearch');
-
 Route::get('/','FrontendController@index')->name('home');
-Route::get(trans('routes.object').'/{id}', 'FrontendController@object')->name('object');
-Route::get(trans('routes.roomsearch'), 'FrontendController@roomsearch')->name('roomSearch');
-Route::get(trans('routes.room'), 'FrontendController@room')->name('room');
-Route::get(trans('routes.article'), 'FrontendController@article')->name('article');
-Route::get(trans('routes.person'), 'FrontendController@person')->name('person');
+Route::get(trans('routes.object').'/{id}','FrontendController@object')->name('object');
+Route::get(trans('routes.roomsearch'),'FrontendController@roomsearch')->name('roomSearch');
+Route::get(trans('routes.room'),'FrontendController@room')->name('room');
+Route::get(trans('routes.article'),'FrontendController@article')->name('article');
+Route::get(trans('routes.person'),'FrontendController@person')->name('person');
+Route::get('/searchCities', 'FrontendController@searchCities');
 
+Route::group(['prefix'=>'admin','middleware'=>'auth'],function(){
 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function (){
-
-    Route::get('/', 'BackendController@index')->name('adminHome');
-    Route::get(trans('routes.myobjects'), 'BackendController@myobjects')->name('myObjects');
-    Route::get(trans('routes.saveobject'), 'BackendController@saveObject')->name('saveObject');
-    Route::get(trans('routes.profile'), 'BackendController@profile')->name('profile');
-    Route::get(trans('routes.saveroom'), 'BackendController@saveRoom')->name('saveRoom');
-    Route::get('/cities', 'BackendController@cities')->name('cities.index');
+    Route::get('/','BackendController@index')->name('adminHome');
+    Route::get(trans('routes.myobjects'),'BackendController@myobjects')->name('myObjects');
+    Route::get(trans('routes.saveobject'),'BackendController@saveObject')->name('saveObject');
+    Route::get(trans('routes.profile'),'BackendController@profile')->name('profile');
+    Route::get(trans('routes.saveroom'),'BackendController@saveRoom')->name('saveRoom');
+    Route::get('/cities','BackendController@cities')->name('cities.index');
 
 
 });
 
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');  /* Lecture 7 */
