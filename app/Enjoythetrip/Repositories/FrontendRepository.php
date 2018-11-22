d77fd10143a53f79ca0e592537362d8551e50b47
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-11-22 11:54:57 +0100

    get room by id, get reservations for room

--- app/Enjoythetrip/Repositories/FrontendRepository.php
+++ app/Enjoythetrip/Repositories/FrontendRepository.php
@@ -3,7 +3,7 @@
 namespace App\Enjoythetrip\Repositories;
 
 use App\Enjoythetrip\Interfaces\FrontendRepositoryInterface;
-use App\{TouristObject,City};
+use App\{Reservation, TouristObject, City, Room};
 
 class FrontendRepository implements FrontendRepositoryInterface  {
 
@@ -13,18 +13,13 @@ class FrontendRepository implements FrontendRepositoryInterface  {
         return TouristObject::with(['city','photos'])->ordered()->paginate(8);
     }
 
-
-
     public function getObject($id)
     {
         //return TouristObject::find($id);
 
-
         // rooms.object.city   for json mobile because there is no lazy loading there
         return  TouristObject::with(['city','photos', 'address','users.photos','rooms.photos','comments.user','articles.user','rooms.object.city'])->find($id);
     }
-
-
 
     public function getSearchCities( string $term)
     {
@@ -35,6 +30,18 @@ class FrontendRepository implements FrontendRepositoryInterface  {
 
         return City::with(['rooms.reservations', 'rooms.photos', 'rooms.object.photos'])->where('name', $city)->first() ?? false;
 
+    }
+
+    public function getRoom($id) {
+
+        return Room::with(['object.address'])->find($id);
+
+    }
+
+    public function getReservationsByRoomId($room_id)
+    {
+
+        return Reservation::where('room_id', $room_id)->get();
     }
 
 
