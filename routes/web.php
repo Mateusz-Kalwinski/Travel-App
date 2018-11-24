423f25fd302e07998e22c5d6ef9d2bddcdafca46
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-11-24 09:16:44 +0100

    route to like, unlike and addComment with like/comment able and type (Article, TouristObject) arguments

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,28 @@
 <?php
 
 Route::get('/','FrontendController@index')->name('home');
+
 Route::get(trans('routes.object').'/{id}','FrontendController@object')->name('object');
+
 Route::post(trans('routes.roomsearch'),'FrontendController@roomsearch')->name('roomSearch');
+
 Route::get(trans('routes.room'). '/{id}','FrontendController@room')->name('room');
-Route::get(trans('routes.article'),'FrontendController@article')->name('article');
-Route::get(trans('routes.person'),'FrontendController@person')->name('person');
+
+Route::get(trans('routes.article'). '/{id}','FrontendController@article')->name('article');
+
+Route::get(trans('routes.person'). '/{id}','FrontendController@person')->name('person');
 
 Route::get('/searchCities', 'FrontendController@searchCities');
 
-Route::get('ajaxGetRoomReservations/{id}', 'FrontendController@ajaxGetRoomReservations');
+Route::get('/ajaxGetRoomReservations/{id}', 'FrontendController@ajaxGetRoomReservations');
+
+Route::get('/like/{likeable_id}/{type}', 'FrontendController@like')->name('like');
+
+Route::get('/unlike/{likeable_id}/{type}', 'FrontendController@unlike')->name('unlike');
+
+Route::post('/addComment/{commentable_id}/{type}', 'FrontendController@addComment')->name('addComment');
+
+//ADMIN
 
 Route::group(['prefix'=>'admin','middleware'=>'auth'],function(){
 
