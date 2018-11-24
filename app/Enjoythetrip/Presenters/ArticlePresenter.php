ffd31a1bf5a398cea9a68a49567d89a96204b7f1
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-11-24 09:26:49 +0100

    additional method to the article

---
+++ app/Enjoythetrip/Presenters/ArticlePresenter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enjoythetrip\Presenters;
+
+trait ArticlePresenter {
+
+    public function getLinkAttribute() {
+
+        return route('article', ['id'=>$this->id]);
+
+    }
+
+    public function getTypeAttribute() {
+
+        return $this->title. ' article';
+
+    }
+
+}
