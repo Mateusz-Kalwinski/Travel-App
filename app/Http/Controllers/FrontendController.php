6b4eeb47199dc5660046f8072e98e0bdb2b036eb
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-11-25 16:22:12 +0100

    makeReservation method return json with information about is avaliable reservation

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use http\Env\Response;
 use Illuminate\Http\Request;
 use App\Enjoythetrip\Interfaces\FrontendRepositoryInterface;
 use App\Enjoythetrip\Gateways\FrontendGateway;
@@ -108,6 +109,34 @@ class FrontendController extends Controller
         $this->fG->addComment($commentable_id, $type, $request);
 
         return redirect()->back();
+    }
+
+    public function makeReservation($room_id, $city_id, Request $request) {
+
+        $avaiable = $this->fG->checkAvaiableReservations($room_id, $request);
+
+        if (!$avaiable) {
+
+            if (!$request->ajax()){
+
+                $request->session()->flash('reservationMsg', __('There are no vacancies'));
+                return redirect()->route('room', ['id'=>$room_id, '#reservation']);
+
+            }
+
+            return response()->json(['reservation'=>false]);
+
+        }else {
+
+            $reservation = $this->fG->makeReservation($room_id, $city_id, $request);
+
+            if (!$request->ajax())
+            return redirect()->route('adminHome');
+            else
+                return response()->json(['reservation'=>$reservation]);
+
+        }
+
     }
 
 }
