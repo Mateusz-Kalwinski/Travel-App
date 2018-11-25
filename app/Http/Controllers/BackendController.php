f7f296319bb4b30f6bb270c40ca59bb0e7405b44
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-11-25 16:17:08 +0100

    construct with BackendGateway and BackendRepository. Confirm and delete reservation

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -4,29 +4,80 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Enjoythetrip\Interfaces\BackendRepositoryInterface;
+use App\Enjoythetrip\Gateways\BackendGateway;
+
 class BackendController extends Controller
 {
-    public function index(){
-        return view('backend.index');
+    use \App\Enjoythetrip\Traits\Ajax;
+
+
+    public function __construct(BackendGateway $backendGateway, BackendRepositoryInterface $backendRepository)
+    {
+        $this->bG = $backendGateway;
+        $this->bR = $backendRepository;
     }
 
-    public function cities(){
+
+
+    public function index(Request $request ) {
+        $objects = $this->bG->getReservations($request);
+        return view('backend.index',['objects'=>$objects]);
+    }
+
+
+    public function cities() {
         return view('backend.cities');
     }
 
-    public function myobjects(){
+
+    public function myobjects() {
         return view('backend.myobjects');
     }
 
-    public function profile(){
+
+    public function profile() {
         return view('backend.profile');
     }
 
-    public function saveobject(){
+
+    public function saveobject() {
         return view('backend.saveobject');
     }
 
-    public function saveroom(){
+
+    public function saveroom() {
         return view('backend.saveroom');
     }
+
+
+
+    public function confirmReservation($id) {
+        $reservation = $this->bR->getReservation($id);
+
+        $this->authorize('reservation', $reservation);
+
+        $this->bR->confirmReservation($reservation);
+
+        $this->flashMsg('success', __('Reservation has been confirmed'));
+
+        if (!\Request::ajax())
+            return redirect()->back();
+    }
+
+
+
+    public function deleteReservation($id) {
+        $reservation = $this->bR->getReservation($id);
+
+        $this->authorize('reservation', $reservation);
+
+        $this->bR->deleteReservation($reservation);
+
+        $this->flashMsg('success', __('Reservation has been deleted'));
+
+        if (!\Request::ajax())
+            return redirect()->back();
+    }
+
 }
