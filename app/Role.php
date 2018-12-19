da42eddec806c840e3f04b014a274f5daad2bd04
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-12-19 21:36:44 +0100

    guarded and timestamp set false

--- app/Role.php
+++ app/Role.php
@@ -4,11 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Role extends Model
 {
-    public function users() {
 
+    public $guarded = [];
+    public $timestamps = false;
+
+
+    public function users()
+    {
         return $this->belongsToMany('App\User');
-
     }
 }
