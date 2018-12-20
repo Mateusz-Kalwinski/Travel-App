215e67499e5eb79a5a1e0eaf40547f1fd8bbecea
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-12-20 20:19:03 +0100

    checks the login method. Mobile or web app

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,21 +4,17 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers {
+
+        login as protected webLogin;
+
+    }
 
     /**
      * Where to redirect users after login.
@@ -36,4 +32,36 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+
+
+    public function login(Request $request)
+    {
+
+        if ($request->ajax())
+        {
+            $credentials = $request->only('email', 'password');
+
+            try {
+                // verify the credentials and create a token for the user
+                if (!$token = \Tymon\JWTAuth\Facades\JWTAuth::attempt($credentials))
+                {
+                    return response()->json(['error' => 'invalid_credentials'], 401);
+                }
+            } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
+                // something went wrong
+                return response()->json(['error' => 'could_not_create_token'], 500);
+            }
+
+            $user = Auth::user();
+            $role = $user->roles[0];
+
+            // if no errors are encountered we can return a JWT
+            return response()->json(compact('token','role'));
+        }
+        else
+            return $this->webLogin($request);
+    }
+
+
 }
