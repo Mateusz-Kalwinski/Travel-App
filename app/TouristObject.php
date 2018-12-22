8193707ea461badb69d15deba0a0cde0610dc672
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-12-22 20:11:17 +0100

    comments, articles and likes relations

--- app/TouristObject.php
+++ app/TouristObject.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
@@ -11,53 +10,69 @@ class TouristObject extends Model
 {
 
     protected $table = 'objects';
+    public $timestamps = false;
 
     use Enjoythetrip\Presenters\ObjectPresenter;
 
 
-    public function scopeOrdered($query) {
+    public function scopeOrdered($query)
+    {
         return $query->orderBy('name', 'asc');
     }
 
-    public function city() {
+
+
+    public function city()
+    {
         return $this->belongsTo('App\City');
     }
 
-    public function user(){
+
+    public function user()
+    {
         return $this->belongsTo('App\User');
     }
 
 
-    public function photos() {
+    public function photos()
+    {
         return $this->morphMany('App\Photo', 'photoable');
     }
 
-    public function users() {
+
+    public function users()
+    {
         return $this->morphToMany('App\User', 'likeable');
     }
 
-    public function address() {
+
+    public function address()
+    {
         return $this->hasOne('App\Address','object_id');
     }
 
-    public function rooms() {
+
+    public function rooms()
+    {
         return $this->hasMany('App\Room','object_id');
     }
 
-    public function comments() {
 
+    public function comments()
+    {
         return $this->morphMany('App\Comment', 'commentable');
     }
 
-    public function articles() {
 
+    public function articles()
+    {
         return $this->hasMany('App\Article','object_id');
     }
 
-    public function isLiked() {
 
+    public function isLiked()
+    {
         return $this->users()->where('user_id', Auth::user()->id)->exists();
-
     }
 
 
