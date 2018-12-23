2ff489653ec636a8e3ea835db33e488a49588245
Mateusz-Kalwinski <mateuszkalwinski97@gmail.com>
2018-12-23 20:30:46 +0100

    user notifications

---
+++ app/Enjoythetrip/ViewComposers/BackendComposer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Enjoythetrip\ViewComposers;
+
+use Illuminate\View\View;
+use App\Notification;
+use Illuminate\Support\Facades\Auth;
+
+class BackendComposer
+{
+
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        $view->with('notifications', Notification::where('user_id', Auth::user()->id )->where('status',0)->get());
+    }
+}
